a4154450752a82af838b04bc30486649c1419dd9
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 17:14:45 +0700

    ganteng

---
+++ MyNewCow/MyNewCow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewCow
+{
+  public class MyNewCow
+    {
+        private string name;
+        private int numberofFoot = 4;
+        private int age = 0;
+
+       public void ChangeName(String NewName)
+        {
+            name = NewName;
+        }
+        public string LookingName()
+        {
+            return name;
+        }
+        public int LookingFootNumber()
+        {
+            return numberofFoot;
+        }
+        public int LihatUmur()
+        {
+            return age;
+        }
+        public void Makan()
+        {
+            Console.WriteLine("{0} Sedang Makan :", name);
+        }
+        public void Jalan()
+        {
+            Console.WriteLine("{0} Sedang Jalan :", name);
+
+        }
+        public void Bobo()
+        {
+            Console.WriteLine("{0} Sedang Bobo :", name);
+        }
+    }
+}
---
+++ MyNewCow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewCow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNewCow sapi = new MyNewCow();
+            sapi.ChangeName("Unyil");
+
+            Console.WriteLine("My cow name is {0}.", sapi.LookingName());
+            Console.WriteLine("The number of feet are {0}", sapi.LookingFootNumber());
+            Console.WriteLine("The age is {0}", sapi.LihatUmur());
+
+            sapi.Jalan();
+            sapi.Makan();
+            sapi.Bobo();
+            Console.ReadKey();
+        }
+    }
+}
